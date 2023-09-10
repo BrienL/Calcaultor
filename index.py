fc55c864ac64bbbaf8b47ba67a8977bdb9d3424d
Your Name <BrienLin@gmail.com>
2023-09-11 05:24:47 +1000

    NavBar added

--- index.py
+++ index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route("/simple")
+def simple():
+    return render_template("simple.html")
+
 if __name__ == "__main__":
     app.run(debug=True)

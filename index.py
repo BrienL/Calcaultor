0e5f6bc0e09bcb38fd821357bb026926f25ba584
Your Name <BrienLin@gmail.com>
2023-09-11 05:53:14 +1000

    Working Maths

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,24 @@ def main():
 @app.route("/simple")
 def simple():
     return render_template("simple.html")
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    First_Num = int(request.form["FirstNum"])
+    Operation = request.form["Operation"]
+    Second_Num = int(request.form["SecNum"])
+    if Operation == "plus":
+        result = First_Num + Second_Num
+    elif Operation == "minus":
+        result = First_Num - Second_Num
+    elif Operation == "multiply":
+        result = First_Num * Second_Num
+    elif Operation == "divide": 
+        result = First_Num / Second_Num
+    else:
+        return "There is an error please try again"
+    return render_template("simple.html", result=result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
